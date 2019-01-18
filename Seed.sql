20b5d1fface6129c4758ada7714caee41d6c7941
lidetsn <43561044+lidetsn@users.noreply.github.com>
2019-01-18 09:48:13 -0800

    Update Seed.sql

--- Seed.sql
+++ Seed.sql
@@ -1,10 +1,6 @@
-CREATE DATABASE bamazon;
+
 
 USE bamazon;
-
-CREATE TABLE products(item_id INT AUTO_INCREMENT NOT NULL , product_name VARCHAR(100) NOT NULL,department_name VARCHAR(100) NOT NULL,price DECIMAL(10,2) NOT NULL,stock_quantity INT,product_sales  DECIMAL(10,2) default 0.00, PRIMARY KEY (item_id));
-
-
 
 INSERT INTO products(product_name,department_name,price,stock_quantity)
            VALUES("usb charger","Computers & Accessories",12.45,100),
@@ -20,12 +16,11 @@ INSERT INTO products(product_name,department_name,price,stock_quantity)
                  ("Head Youth Full Zip","Clothing",12.50,50),
                  ("OshKosh Fleece Hoodie","Clothing",10.50,100);
 
-CREATE TABLE departments( department_id INT AUTO_INCREMENT NOT NULL,department_name VARCHAR(100) NOT NULL,over_head_costs DECIMAL(10,2) NOT NULL, PRIMARY KEY(department_id) );
 
 INSERT INTO departments (department_name,over_head_costs)
            
             VALUES("Computers & Accessories",20000),
-				  ("Health & Beauty",15000),
+		  ("Health & Beauty",15000),
                   ("Home & Kitchen",20000),
                   ("Furniture & Mattresses",32000),
                   ("Books",9000),
